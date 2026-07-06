858030e307b3bc4a60bce6071322ec93c4016298
владислав <vladislav@MacBook-Air-vladislav.local>
2026-07-06 23:08:06 +0700

    clean project: remove pycache and sqlite, fix structure

--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('register/', views.register, name='register'),
-    path('profile/', views.profile, name='profile'),
+    path("", views.home, name="home"),  # ← ДОБАВЬ ЭТО
+    path("register/", views.register, name="register"),
+    path("profile/", views.profile, name="profile"),
     path("edit/", views.edit_profile, name="edit_profile"),
 ]
