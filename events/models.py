a23ca5384be8e09cae349ba94d2800d73c48490b
владислав <vladislav@MacBook-Air-vladislav.local>
2026-07-13 19:11:58 +0700

    Add event editing and image upload

--- events/models.py
+++ events/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
+
+
 class Event(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(blank=True)
     date = models.DateTimeField()
     location = models.CharField(max_length=200, blank=True)
+    image = models.ImageField(upload_to='events/', blank=True, null=True)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
--- events/views.py
+++ events/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.utils import timezone
@@ -21,6 +21,7 @@ def create_event(request):
         description = request.POST.get('description')
         date = request.POST.get('date')
         location = request.POST.get('location')
+        image = request.FILES.get('image')
 
         if title and date:
             Event.objects.create(
@@ -28,8 +29,49 @@ def create_event(request):
                 description=description,
                 date=date,
                 location=location,
+                image=image,
                 created_by=request.user
             )
             return redirect('events_list')
 
     return render(request, 'events/create_event.html')
+
+
+@login_required
+def edit_event(request, pk):
+    event = get_object_or_404(Event, pk=pk)
+
+    # Право редактировать есть только у организаторов
+    if not request.user.profile.is_organizer:
+        return redirect('events_list')
+
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        date = request.POST.get('date')
+
+        if title and date:
+            event.title = title
+            event.description = request.POST.get('description')
+            event.date = date
+            event.location = request.POST.get('location')
+
+            image = request.FILES.get('image')
+            if image:
+                event.image = image
+
+            event.save()
+            return redirect('events_list')
+
+    return render(request, 'events/edit_event.html', {'event': event})
+
+
+@login_required
+def delete_event(request, pk):
+    event = get_object_or_404(Event, pk=pk)
+
+    # Удалять может только организатор, и только через POST
+    # (чтобы событие нельзя было случайно удалить одной ссылкой)
+    if request.user.profile.is_organizer and request.method == 'POST':
+        event.delete()
+
+    return redirect('events_list')
